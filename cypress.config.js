4ecdafc18dc91eafdad59da88834fcb0b841cbaf
Maya Maulani <my.maulani@gmail.com>
2025-05-08 21:11:01 +0700

    add mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: true,
+    html: true,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // require("cypress-mochawesome-reporter")(on);
     },
     supportFile: "cypress/support/e2e.js",
     defaultCommandTimeout: 30000, // dalam milisecond (30 detik)
